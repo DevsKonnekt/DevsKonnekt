1ebd03a294c6f8e020af729ac0b55fe63f67e509
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-11-09 14:37:12 +0000

    Update benefits.jsx to align text left on small screens

--- Frontend/components/benefits.jsx
+++ Frontend/components/benefits.jsx
@@ -6,8 +6,8 @@ const Benefits = () => {
   return (
     <section className='px-4 my-8 w-full' >
         <div className='flex flex-col items-center '>
-            <h2 className='text-3xl font-bold text-center text-primary'>DevsKonnekt: Konnekting Developers Like Never Before</h2>
-            <p className='text-center text-primary/80 text-lg font-semibold'>Unlock your development potential with DevsKonnekt.</p>
+            <h2 className='text-3xl font-bold text-start sm:text-center text-primary'>DevsKonnekt: Konnekting Developers Like Never Before</h2>
+            <p className='text-start sm:text-center text-primary/80 text-lg font-semibold'>Unlock your development potential with DevsKonnekt.</p>
         </div>
         <div className="flex flex-col sm:flex-row flex-wrap items-center justify-center gap-4 mt-8 w-full">
           {benefits.map((benefit, index) => (
