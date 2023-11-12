fbed0b2c18449718a26ca06c6cedecd5e3f9d7cb
marrwan <abdulbasitdamilola6@gmail.com>
2023-11-12 22:05:33 +0100

    controller logic updated

--- Backend/controllers/messages.js
+++ Backend/controllers/messages.js
@@ -1,5 +1,5 @@
 import Message from "../models/message.js";
-
+import User from "../models/users.js";
 /**
  * @module controllers/messages.js
  * @requires models/message.js
@@ -12,11 +12,29 @@ import Message from "../models/message.js";
  * @description Create a new message in the database
  */
 export const createMessage = async (req, res) => {
-    const messageData = req.body;
-    const newMessage = new Message(messageData);
     try {
+        const { senderId, receiverId, content } = req.body;
+        if (!senderId || !receiverId || !content) {
+            return res.status(400).json({ message: "Sender, receiver, and content are required fields." });
+        }
+
+        // Check if sender and receiver exist
+        const sender = await User.findById(senderId);
+        const receiver = await User.findById(receiverId);
+        if (!sender || !receiver) {
+            return res.status(404).json({ message: "Sender or receiver not found." });
+        }
+
+        // Create and save the message
+        const newMessage = new Message({
+            sender: senderId,
+            receiver: receiverId,
+            content,
+        });
+
         await newMessage.save();
         res.status(201).json(newMessage);
+    
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
@@ -29,7 +47,7 @@ export const createMessage = async (req, res) => {
  */
 export const getAllMessages = async (req, res) => {
     try {
-        const messages = await Message.find();
+        const messages = await Message.find().populate("sender receiver"); // Populate sender and receiver details
         res.status(200).json(messages);
     } catch (error) {
         res.status(404).json({ message: error.message });
@@ -42,9 +60,14 @@ export const getAllMessages = async (req, res) => {
  * @description Get a single message from the database
  */
 export const getMessage = async (req, res) => {
-    const { id } = req.params;
     try {
-        const message = await Message.findById(id);
+        const { id } = req.params;
+        const message = await Message.findById(id).populate("sender receiver"); // Populate sender and receiver details
+
+        if (!message) {
+            return res.status(404).json({ message: "Message not found." });
+        }
+
         res.status(200).json(message);
     } catch (error) {
         res.status(404).json({ message: error.message });
@@ -57,9 +80,26 @@ export const getMessage = async (req, res) => {
  * @description Find and update a message in the database
  */
 export const updateMessage = async (req, res) => {
-    const { id } = req.params;
     try {
-        const updatedMessage = await Message.findOneAndUpdate({ _id: id }, { ...req.body }, { new: true });
+        const { id } = req.params;
+
+        // Validate input
+        const { content } = req.body;
+        if (!content) {
+            return res.status(400).json({ message: "Content is a required field for updating a message." });
+        }
+
+        // Update the content of the message
+        const updatedMessage = await Message.findByIdAndUpdate(
+            id,
+            { content },
+            { new: true }
+        ).populate("sender receiver"); // Populate sender and receiver details
+
+        if (!updatedMessage) {
+            return res.status(404).json({ message: "Message not found." });
+        }
+
         res.status(200).json(updatedMessage);
     } catch (error) {
         res.status(404).json({ message: error.message });
@@ -72,9 +112,16 @@ export const updateMessage = async (req, res) => {
  * @description Find and delete a message from the database
  */ 
 export const deleteMessage = async (req, res) => {
-    const { id } = req.params;
     try {
-        await Message.findByIdAndDelete(id);
+        const { id } = req.params;
+
+        // Find and delete the message
+        const deletedMessage = await Message.findByIdAndDelete(id);
+
+        if (!deletedMessage) {
+            return res.status(404).json({ message: "Message not found." });
+        }
+
         res.status(204).json();
     } catch (error) {
         res.status(404).json({ message: error.message });
--- Backend/models/message.js
+++ Backend/models/message.js
@@ -8,24 +8,24 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', // Assuming you have a User model
-    required: true,
-  },
-  receiver: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  content: {
-    type: String,
-    required: [true, "Content is required"],
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now,
-  },
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", // Assuming you have a User model
+        required: true,
+    },
+    receiver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    content: {
+        type: String,
+        required: [true, "Content is required"],
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    },
 }, { autoIndex: false });
 
 const Message = mongoose.model("Message", messageSchema);
