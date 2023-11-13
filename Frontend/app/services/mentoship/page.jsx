4d83a549c70e6583addeefd5a9b6e2ae49b019e9
SirSanctified <mambambopritchard@gmail.com>
2023-11-13 14:46:27 +0200

    Add Description component to Mentoship page and update Hero styles

--- Frontend/app/services/mentoship/page.jsx
+++ Frontend/app/services/mentoship/page.jsx
@@ -1,9 +1,11 @@
+import Description from "@/components/services/mentorship/description";
 import Hero from "@/components/services/mentorship/hero";
 import React from "react";
 
 const Mentoship = () => {
   return <div>
     <Hero />
+    <Description />
   </div>;
 };
 
