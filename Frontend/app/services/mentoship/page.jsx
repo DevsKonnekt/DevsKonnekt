2f5b19a17665181b2660aa8e06d0a378a8ac8d3d
SirSanctified <mambambopritchard@gmail.com>
2023-11-13 15:57:22 +0200

    Add Why component and fix formatting in Mentors component

--- Frontend/app/services/mentoship/page.jsx
+++ Frontend/app/services/mentoship/page.jsx
@@ -2,6 +2,7 @@ import Description from "@/components/services/mentorship/description";
 import Hero from "@/components/services/mentorship/hero";
 import Mentees from "@/components/services/mentorship/mentees";
 import Mentors from "@/components/services/mentorship/mentors";
+import Why from "@/components/services/mentorship/why";
 import React from "react";
 
 const Mentoship = () => {
@@ -11,6 +12,7 @@ const Mentoship = () => {
       <Description />
       <Mentees />
       <Mentors />
+      <Why />
     </div>
   );
 };
