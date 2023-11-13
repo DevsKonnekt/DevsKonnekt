10f39faae49c3057aa8548bafccdfebc1e9663dd
SirSanctified <mambambopritchard@gmail.com>
2023-11-13 14:46:27 +0200

    Add Description component to Mentoship page and update Hero styles

---
+++ Frontend/components/services/mentorship/description.jsx
@@ -0,0 +1,29 @@
+const Description = () => {
+  return (
+    <div className="max-w-5xl mx-4 md:mx-8 lg:mx-auto w-full flex flex-col items-start justify-start mt-8">
+      <h2 className="text-2xl font-bold text-start mb-6 w-full">
+        Unlock your potential and help others grow in the coding realm
+      </h2>
+      <p className="pr-6 text-start mb-2">
+        At DevsKonnekt, we understand the importance of mentorship in the
+        software development community. That's why we provide a platform where
+        you can find experienced mentors who are eager to share their knowledge
+        and guide you on your coding journey. Whether you're a seasoned
+        developer looking to expand your skills or just starting out and seeking
+        guidance, our mentorship program is designed to connect you with
+        professionals who can help you reach your goals.
+      </p>
+      <p className="pr-6 text-start">
+        Our mentors are carefully selected based on their expertise and
+        experience in various programming languages, frameworks, and
+        technologies. They are passionate about helping others succeed and are
+        committed to providing valuable insights, advice, and support. Through
+        one-on-one sessions, you'll have the opportunity to ask questions,
+        receive personalized guidance, and gain practical knowledge from
+        industry experts.
+      </p>
+    </div>
+  );
+};
+
+export default Description;
