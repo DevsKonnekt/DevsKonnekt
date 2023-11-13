3575f66926361ab021c7c9f9f99aa3e87da4cc48
SirSanctified <mambambopritchard@gmail.com>
2023-11-13 14:31:26 +0200

    Add hero section for mentoship service page

---
+++ Frontend/app/services/mentoship/page.jsx
@@ -0,0 +1,10 @@
+import Hero from "@/components/services/mentorship/hero";
+import React from "react";
+
+const Mentoship = () => {
+  return <div>
+    <Hero />
+  </div>;
+};
+
+export default Mentoship;
