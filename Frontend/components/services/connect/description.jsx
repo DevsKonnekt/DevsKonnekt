35f609a6688d6c438791932c0e9a9ab089f5d3b2
SirSanctified <mambambopritchard@gmail.com>
2023-11-15 12:00:14 +0200

    Refactor code for better readability and consistency.

--- Frontend/components/services/connect/description.jsx
+++ Frontend/components/services/connect/description.jsx
@@ -7,13 +7,28 @@ const Description = () => {
         Explore the opportunities down the block
       </h1>
       <p className="text-lg sm:text-xl text-primary/80 font-semibold mb-4">
-      Say goodbye to mundane connections and hello to a world of exciting coding encounters.
+        Say goodbye to mundane connections and hello to a world of exciting
+        coding encounters.
       </p>
       <p className="max-w-5xl font-medium">
-      Whether you&#39;re a seasoned professional or just starting out in your career, our platform provides the perfect space for you to connect, collaborate, and grow. With an informal tone and user-friendly interface, you&#39;ll feel right at home as you navigate through our virtual networking community.
+        Whether you&#39;re a seasoned professional or just starting out in your
+        career, our platform provides the perfect space for you to connect,
+        collaborate, and grow. With an informal tone and user-friendly
+        interface, you&#39;ll feel right at home as you navigate through our
+        virtual networking community.
       </p>
       <p className="max-w-5xl font-medium">
-      Joining our Networking Platform gives you access to a wide range of features designed to enhance your networking experience. From customizable profiles and interactive forums to virtual events and job boards, there&#39;s something for everyone. Connect with industry leaders, join interest groups, and even find mentors to guide you on your professional journey. With our informal tone, you&#39;ll feel comfortable reaching out to others and making meaningful connections. Say goodbye to stuffy corporate networking events and hello to a vibrant and dynamic online community that&#39;s always buzzing with new opportunities. So why wait? Sign up today and start expanding your professional network in a fun and informal way!
+        Joining our Networking Platform gives you access to a wide range of
+        features designed to enhance your networking experience. From
+        customizable profiles and interactive forums to virtual events and job
+        boards, there&#39;s something for everyone. Connect with industry
+        leaders, join interest groups, and even find mentors to guide you on
+        your professional journey. With our informal tone, you&#39;ll feel
+        comfortable reaching out to others and making meaningful connections.
+        Say goodbye to stuffy corporate networking events and hello to a vibrant
+        and dynamic online community that&#39;s always buzzing with new
+        opportunities. So why wait? Sign up today and start expanding your
+        professional network in a fun and informal way!
       </p>
     </section>
   );
