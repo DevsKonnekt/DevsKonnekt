bcb37b7c12c0d856a700a0a84760b78e25f02711
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-11-15 12:03:28 +0000

    Add padding to CTA section

--- Frontend/components/services/cta.jsx
+++ Frontend/components/services/cta.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function CTA() {
   return (
-    <section className="w-full bg-background py-8 flex flex-col justify-center items-center">
+    <section className="w-full bg-background py-8 flex flex-col justify-center items-center px-4">
       <h1 className="text-5xl text-primary font-bold mb-4">Join Today</h1>
       <p className="text-primary text-center max-w-lg overflow-hidden line-clamp-4 mt-8">
         Get instant access to all of our amazing features! Start collaborating
