a42e4526b4a13692cda0d88e52fc57129f6812f7
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-11-20 19:46:22 +0000

    Add log rotation to winston logger

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,11 @@ import express, { json } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
-import logger from "./config/logger.js";
+import logger, { winstonLoggerStream } from "./config/logger.js";
 import { connectDB } from "./config/db.js";
 import userRouter from "./routes/users.js";
 import productServiceRouter from "./routes/productservice.js";
-
-
+import { defaultErrorHandler } from "./middlewares/index.js";
 
 dotenv.config();
 
@@ -17,17 +16,14 @@ connectDB();
 const app = express();
 app.use(json());
 app.use(cors());
+app.use(morgan("combined", { stream: winstonLoggerStream }));
 app.use(morgan("combined"));
 
-app.use("/users", userRouter);
-app.use("/productservices", productServiceRouter);
-
-logger.info("Logging something");
+app.use("/users", defaultErrorHandler, userRouter);
+app.use("/productservices", defaultErrorHandler, productServiceRouter);
 
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    logger.info(`Server is running on port ${port}`);
+  logger.info(`Server is running on port ${port}`);
 });
-
-
--- Backend/middlewares/middleware.js
+++
@@ -1,8 +0,0 @@
-// middleware/customMiddleware.js
-const customMiddleware = (req, res, next) => {
-    // Custom middleware logic
-    next();
-  };
-  
-  export default customMiddleware;
-  
