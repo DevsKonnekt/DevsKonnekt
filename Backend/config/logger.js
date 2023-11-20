af7e5b43bae8b778f918fecf0aab5839ed7cc561
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-11-20 19:46:22 +0000

    Add log rotation to winston logger

--- Backend/config/logger.js
+++ Backend/config/logger.js
@@ -1,41 +1,122 @@
-import { createLogger, format, transports } from "winston";
+import winston, { createLogger, format, transports } from "winston";
+import * as rotate from "winston-daily-rotate-file";
 import path from "path";
 import { mkdirSync } from "fs";
 import process from "process";
 
-const __filename = new URL(import.meta.url).pathname;
-const __dirname = path.dirname(__filename);
-const logsDir = path.join(__dirname, "logs");
+const logsDir = path.join(process.cwd(), "logs");
 
 mkdirSync(logsDir, { recursive: true });
 
-const logger = createLogger({
-    level: "info",
-    format: format.combine(
-        format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-        format.errors({ stack: true }),
-        format.splat(),
-        format.json(),
-    ),
-    transports: [
-        new transports.File({
-            // eslint-disable-next-line no-undef
-            filename: path.join(logsDir, "error.log"), // Specify the directory for log files
-            level: "error",
-        }),
-        // eslint-disable-next-line no-undef
-        new transports.File({ filename: path.join(logsDir, "combined.log") }),
-    ],
+const errorLog = path.join(logsDir, `errors-`);
+const combinedLog = path.join(logsDir, `logs-`);
+const exceptionLog = path.join(
+  logsDir,
+  `exceptions-${new Date().toISOString().split("T")[0]}.log`
+);
+const rejectionLog = path.join(
+  logsDir,
+  `rejections-${new Date().toISOString().split("T")[0]}.log`
+);
+const { combine, timestamp, json } = format;
 
+const errorFileRotateTransport = new winston.transports.DailyRotateFile({
+  filename: errorLog + "%DATE%.log",
+  datePattern: "YYYY-MM-DD",
+  maxFiles: "30d",
+  level: "error",
+});
+const combinedFileRotateTransport = new winston.transports.DailyRotateFile({
+  filename: combinedLog + "%DATE%.log",
+  datePattern: "YYYY-MM-DD",
+  maxFiles: "30d",
+});
+
+const logger = createLogger({
+  level: process.env.NODE_ENV === "production" ? "info" : "debug",
+  format: combine(
+    timestamp({
+      format: "YYYY-MM-DD HH:mm:ss SSS A",
+    }),
+    json(
+      {
+        space: 2,
+      },
+      {
+        replacer: (key, value) => {
+          if (key === "password") {
+            return "********";
+          }
+          return value;
+        },
+      }
+    )
+  ),
+  transports: [errorFileRotateTransport, combinedFileRotateTransport],
+  exceptionHandlers: [new winston.transports.File({ filename: exceptionLog })],
+  rejectionHandlers: [new winston.transports.File({ filename: rejectionLog })],
 });
 
 if (process.env.NODE_ENV !== "production") {
-    logger.add(new transports.Console({
-        format: format.combine(
-            format.colorize(),
-            format.simple()
-        ),
-    }));
+  logger.add(
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple()),
+    })
+  );
 }
+
+/**
+ * @function logging
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next function.
+ * @returns {void}
+ * @description logs the message to the logs file.
+ */
+export const logging = (req, res, next) => {
+  logger.info(
+    `${req.method}::${req.originalUrl}::${res.statusCode} - source: ${req.ip}`
+  );
+  next();
+};
+
+/**
+ * @constant {object} winstonLoggerStream - The winston logger stream.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ * @description The winston logger stream.
+ */
+export const winstonLoggerStream = (logger.stream = {
+  write: function (message) {
+    if (message.includes("error")) {
+      winstonLogger.error(message.trim());
+    } else {
+      winstonLogger.info(message.trim());
+    }
+  },
+});
+
+/**
+ * @function errorLogger
+ * @param {Object} err - The error object.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next function.
+ * @returns {void}
+ * @description logs the error to the logs file
+ */
+export const errorLogger = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  const errorResponse = {
+    error: {
+      message: message,
+    },
+  };
+  logger.error(
+    `${req?.method}::${req?.originalUrl}::${statusCode} - source: ${req?.ip} => ${errorResponse.error.message}`
+  );
+  next && next();
+};
 
 export default logger;
--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ import express, { json } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
-import logger from "./config/logger.js";
+import logger, { winstonLoggerStream } from "./config/logger.js";
 import { connectDB } from "./config/db.js";
 import userRouter from "./routes/users.js";
 import productServiceRouter from "./routes/productservice.js";
@@ -22,6 +22,7 @@ connectDB();
 const app = express();
 app.use(json());
 app.use(cors());
+app.use(morgan("combined", { stream: winstonLoggerStream }));
 app.use(morgan("combined"));
 
 // Routes
@@ -40,6 +41,5 @@ logger.info("Logging something");
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   logger.info(`Server is running on port ${port}`);
+  logger.info(`Server is running on port ${port}`);
 });
-
-
