9cac9768d04e825e21c11d3daebeac87c79569e2
SirSanctified <mambambopritchard@gmail.com>
2023-11-21 13:38:12 +0200

    Remove unused mentorshipBenefits constant

--- Frontend/constants/index.js
+++ Frontend/constants/index.js
@@ -108,28 +108,6 @@ export const testimonials = [
 
 ];
 
-export const mentorshipBenefits = [
-  {
-    icon: FaStar,
-    heading: "Connect with Experienced Mentors",
-    text: "Gain valuable insights and guidance from industry professionals",
-  },
-  {
-    icon: MdCheckCircle,
-    heading: "Personalized One-on-One Sessions",
-    text: "Receive individualized support tailored to your specific needs",
-  },
-  {
-    icon: FaHandshake,
-    heading: "Expand Your Network",
-    text: "Connect with like-minded professionals and expand your professional circle",
-  },
-  {
-    icon: FaShapes,
-    heading: "Share Your Knowledge",
-    text: "Guide others on their coding journey and make a positive impact in the developer community",
-  },
-];
 
 export const mentorshipBenefits = [
   {
