b330e9d4337f082ce94e183ad293cd64b19c7e3c
SirSanctified <mambambopritchard@gmail.com>
2024-02-03 14:05:17 +0200

    Add coding challenges page and hero component

---
+++ Frontend/app/services/coding-challenges/page.jsx
@@ -0,0 +1,21 @@
+import BattleTypes from "@/components/services/coding-challenges/battle-types";
+import CTA from "@/components/services/coding-challenges/cta";
+import Hero from "@/components/services/coding-challenges/hero";
+import LevelsAndAchievements from "@/components/services/coding-challenges/levels-and-achievements";
+import Unleash from "@/components/services/coding-challenges/unleash";
+import WhyUs from "@/components/services/coding-challenges/whyUs";
+
+const CodingChallenges = () => {
+  return (
+    <main className="flex flex-col gap-4 w-full mx-auto px-4">
+      <Hero />
+      <BattleTypes />
+      <WhyUs />
+      <LevelsAndAchievements />
+      <Unleash />
+      <CTA />
+    </main>
+  );
+};
+
+export default CodingChallenges;
