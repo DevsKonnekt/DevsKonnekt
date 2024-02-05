f494bf99d16fdacf211e9741d0c7d798a9c48133
SirSanctified <mambambopritchard@gmail.com>
2024-02-05 15:50:06 +0200

    Update imports and routes

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,6 @@ import { connectDB } from "./config/db.js";
 import userRouter from "./routes/users.js";
 import productServiceRouter from "./routes/productservice.js";
 import { defaultErrorHandler } from "./middlewares/index.js";
-import authRoutes from "./routes/auth.js";
 import categoriesRouter from "./routes/categories.js";
 import eventsRoutes from "./routes/events.js";
 import reviewsRouter from "./routes/reviews.js";
@@ -19,12 +18,11 @@ import postsRoutes from "./routes/posts.js";
 import commentsRouter from "./routes/comments.js";
 import bookmarksRouter from "./routes/bookmarks.js";
 
-
 dotenv.config();
 
 connectDB();
 
-const app = express(); 
+const app = express();
 app.use(json());
 app.use(cors());
 app.use(cookieParser());
@@ -32,13 +30,11 @@ app.use(morgan("combined", { stream: winstonLoggerStream }));
 app.use(morgan("combined"));
 
 // Routes
-app.use("/api/v1/auth", defaultErrorHandler, authRoutes);
 app.use("/api/v1/", defaultErrorHandler, categoriesRouter);
 app.use("/api/v1/", defaultErrorHandler, eventsRoutes);
 app.use("/api/v1/", defaultErrorHandler, reviewsRouter);
 app.use("/api/v1/", defaultErrorHandler, venuesRouter);
-app.use("/api/v1/auth/", defaultErrorHandler, authRoutes);
-app.use("/api/v1//users/", defaultErrorHandler, userRouter);
+app.use("/api/v1/users/", defaultErrorHandler, userRouter);
 app.use("/api/v1/productservices/", defaultErrorHandler, productServiceRouter);
 app.use("/api/v1/projects/", defaultErrorHandler, projectRouter);
 app.use("/api/v1/posts/", defaultErrorHandler, postsRoutes);
@@ -46,7 +42,7 @@ app.use("/api/v1/comments/", defaultErrorHandler, commentsRouter);
 app.use("/api/v1/bookmarks/", defaultErrorHandler, bookmarksRouter);
 
 // eslint-disable-next-line no-undef
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   logger.info(`Server is running on port ${port}`);
 });
