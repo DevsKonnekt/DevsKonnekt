1c868ae3787bc8cacdc34b40c788795c33cc47ec
SirSanctified <mambambopritchard@gmail.com>
2024-02-06 13:29:18 +0200

    Remove svix header verification

--- Frontend/app/api/webhooks/clerk/route.js
+++ Frontend/app/api/webhooks/clerk/route.js
@@ -1,5 +1,3 @@
-import { Webhook } from "svix";
-import { headers } from "next/headers";
 import { clerkClient } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
@@ -14,41 +12,8 @@ export async function POST(req) {
     );
   }
 
-  // Get the headers
-  const headerPayload = headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
-
-  // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
-  }
-
   // Get the body
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
-
-  // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
-
-  let evt;
-
-  // Verify the payload with the headers
-  try {
-    evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
-    });
-  } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
-      status: 400,
-    });
-  }
+  const evt = await req.json();
 
   const eventType = evt.type;
 
