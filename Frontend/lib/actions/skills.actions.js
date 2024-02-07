bafec734dc26ce4da8b8845150f386ab99deae0d
SirSanctified <mambambopritchard@gmail.com>
2024-02-07 14:33:05 +0200

    Add skill actions for frontend

---
+++ Frontend/lib/actions/skills.actions.js
@@ -0,0 +1,23 @@
+"use server";
+
+export const addSkill = async (skill) => {
+  try {
+    const response = await axios.post(`${process.env.BACKEND_URL}/skills/`, {
+      name: skill,
+    });
+    return JSON.parse(JSON.stringify(response.data));
+  } catch (error) {
+    throw new Error(typeof error === "string" ? error : JSON.stringify(error));
+  }
+};
+
+export const getAllSkills = async (query) => {
+  try {
+    const response = await axios.get(
+      `${process.env.BACKEND_URL}/skills/?name=${query || ""}`
+    );
+    return JSON.parse(JSON.stringify(response.data));
+  } catch (error) {
+    throw new Error(typeof error === "string" ? error : JSON.stringify(error));
+  }
+};
