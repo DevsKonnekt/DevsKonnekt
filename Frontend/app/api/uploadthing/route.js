f912df4082f18b2742032b2473abae908d6b4dbb
SirSanctified <mambambopritchard@gmail.com>
2024-02-08 10:36:15 +0200

    Add UploadThing components and configuration

---
+++ Frontend/app/api/uploadthing/route.js
@@ -0,0 +1,11 @@
+import { createRouteHandler } from "uploadthing/next";
+import { fileRouter } from "./core";
+
+export const { GET, POST } = createRouteHandler({
+  router: fileRouter,
+  config: {
+    uploadthingId: process.env.UPLOADTHING_APP_ID,
+    uploadthingSecret: process.env.UPLOADTHING_KEY,
+    isDev: process.env.NODE_ENV === "development",
+  },
+});
