4a1243965ff761028f0e02ed99e258aa2af2d162
SirSanctified <mambambopritchard@gmail.com>
2024-02-09 12:15:38 +0200

    Add revalidation of path after creating, updating, and deleting projects

--- Frontend/lib/actions/projects.actions.js
+++ Frontend/lib/actions/projects.actions.js
@@ -1,6 +1,7 @@
 "use server";
 
 import axios from "axios";
+import { revalidatePath } from "next/cache";
 
 export const getProjects = async () => {
   try {
@@ -22,35 +23,38 @@ export const getProject = async (id) => {
   }
 };
 
-export const createProject = async (project) => {
+export const createProject = async ({ project, path }) => {
   try {
     const response = await axios.post(
       `${process.env.BACKEND_URL}/projects/`,
       project
     );
+    revalidatePath(path);
     return JSON.parse(JSON.stringify(response.data));
   } catch (error) {
     throw new Error(typeof error === "string" ? error : JSON.stringify(error));
   }
 };
 
-export const updateProject = async (id, project) => {
+export const updateProject = async (id, project, path) => {
   try {
     const response = await axios.put(
       `${process.env.BACKEND_URL}/projects/${id}/${project.owner}/`,
       project
     );
+    revalidatePath(path);
     return JSON.parse(JSON.stringify(response.data));
   } catch (error) {
     throw new Error(typeof error === "string" ? error : JSON.stringify(error));
   }
 };
 
-export const deleteProject = async (id, owner) => {
+export const deleteProject = async (id, owner, path) => {
   try {
     const response = await axios.delete(
       `${process.env.BACKEND_URL}/projects/${id}/${owner}/`
     );
+    revalidatePath(path);
     return JSON.parse(JSON.stringify(response.data));
   } catch (error) {
     throw new Error(typeof error === "string" ? error : JSON.stringify(error));
