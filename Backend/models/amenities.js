b303715f36cdf3cb489af0cab21ab23bf47014b9
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-02-15 14:04:59 +0200

    Remove required field for description in amenitySchema

--- Backend/models/amenities.js
+++ Backend/models/amenities.js
@@ -24,7 +24,6 @@ const amenitySchema = new mongoose.Schema(
     },
     description: {
       type: String,
-      required: [true, "Description is required"],
     },
   },
   { timestamps: true }
