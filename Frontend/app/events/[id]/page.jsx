b90b054f0e25bfe84bc942346b79fc68fa15d69d
SirSanctified <mambambopritchard@gmail.com>
2024-02-23 11:36:51 +0200

    Refactor event detail page and import components

--- Frontend/app/events/[id]/page.jsx
+++ Frontend/app/events/[id]/page.jsx
@@ -1,7 +1,35 @@
-const EventDetail = () => {
+import EventDetail from "@/components/community/events/eventDetail";
+import EventsList from "@/components/community/events/eventsList";
+import { getEvent, getEventsByCategory } from "@/lib/actions/events.actions";
+import { currentUser } from "@clerk/nextjs";
+
+const EventDetailPage = async ({ params }) => {
+  const { id } = params;
+  const event = await getEvent(id);
+  if (!event) {
+    return {
+      redirect: {
+        destination: "/events",
+        permanent: false,
+      },
+    };
+  }
+  const relatedEvents = await getEventsByCategory(event.category._id);
+  const user = await currentUser();
   return (
-    <main className="w-full min-h-screen px-4 pt-24 md:pt-12">EventDetail</main>
+    <main className="w-full min-h-screen px-4 pt-24 md:pt-12">
+      <EventDetail event={event} user={user} />
+      <div className="w-full mt-4">
+        <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-center w-full">
+          Related Events
+        </h1>
+        <EventsList
+          events={relatedEvents}
+          userId={user?.publicMetadata?.userId}
+        />
+      </div>
+    </main>
   );
 };
 
-export default EventDetail;
+export default EventDetailPage;
