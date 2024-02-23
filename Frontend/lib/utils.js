712f988205678bcde857eba8dfbfa94a8edf4409
SirSanctified <mambambopritchard@gmail.com>
2024-02-23 15:02:12 +0200

    Add getStripe function to utils.js

--- Frontend/lib/utils.js
+++ Frontend/lib/utils.js
@@ -1,5 +1,14 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { loadStripe } from "@stripe/stripe-js";
+
+let stripePromise;
+export const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+  }
+  return stripePromise;
+};
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
