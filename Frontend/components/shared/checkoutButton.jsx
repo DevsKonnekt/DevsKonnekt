f09e09d946a993b00df485b049cd2b6809ffca58
SirSanctified <mambambopritchard@gmail.com>
2024-02-24 17:17:21 +0200

    Refactor checkout process and integrate with user data

--- Frontend/components/shared/checkoutButton.jsx
+++ Frontend/components/shared/checkoutButton.jsx
@@ -2,7 +2,7 @@ import { SignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import Checkout from "./checkout";
 import { Button } from "../ui/button";
 
-const CheckoutButton = ({ event, userId }) => {
+const CheckoutButton = ({ event }) => {
   const hasEventEnded = new Date(event?.endDate) < new Date();
   return (
     <div className="flex items-center justify-start">
@@ -13,7 +13,7 @@ const CheckoutButton = ({ event, userId }) => {
       ) : (
         <>
           <SignedIn>
-            <Checkout event={event} userId={userId} />
+            <Checkout event={event} />
           </SignedIn>
           <SignedOut>
             <SignIn mode="modal">
