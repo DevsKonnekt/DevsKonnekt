6644b725b011b65b41783e582970670ec3f10adf
SirSanctified <mambambopritchard@gmail.com>
2024-02-24 15:08:00 +0200

    Add event tickets page and update profile details page

---
+++ Frontend/app/profile/[id]/event-tickets/page.jsx
@@ -0,0 +1,25 @@
+import EventsList from "@/components/community/events/eventsList";
+import { getTicketsByBuyer } from "@/lib/actions/tickets.actions";
+import { currentUser } from "@clerk/nextjs";
+
+const MyTicketsPage = async () => {
+  const user = await currentUser();
+  const myTickets = await getTicketsByBuyer({
+    buyerId: user?.publicMetadata?.userId,
+  });
+  if (!myTickets) {
+    return <div>No tickets found</div>;
+  }
+  const eventsBought = myTickets.map((ticket) => ticket.event);
+  return (
+    <main className="w-full px-4 pt-32">
+      <EventsList
+        initialEvents={eventsBought}
+        type={"tickets"}
+        userId={user?.publicMetadata?.userId}
+      />
+    </main>
+  );
+};
+
+export default MyTicketsPage;
